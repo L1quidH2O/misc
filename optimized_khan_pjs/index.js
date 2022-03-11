57e685a1a6343913c4e61c9ead755ff13193fb41
L1quidH2O <85402740+L1quidH2O@users.noreply.github.com>
2022-03-11 22:03:24 +0300

    Update index.js

--- optimized_khan_pjs/index.js
+++ optimized_khan_pjs/index.js
@@ -8,11 +8,11 @@
   };
   
   Promise.all([
-    l("./src/core_deps.js"),
-    l("./src/shared.js"),
-    l("./src/output_pjs_deps.js"),
-    l("./src/output.js"),
-    l("./src/output_pjs.js")
+    l("https://cdn.jsdelivr.net/gh/L1quidH2O/misc/optimized_khan_pjs/src/core_deps.js"),
+    l("https://cdn.jsdelivr.net/gh/L1quidH2O/misc/optimized_khan_pjs/src/shared.js"),
+    l("https://cdn.jsdelivr.net/gh/L1quidH2O/misc/optimized_khan_pjs/src/output_pjs_deps.js"),
+    l("https://cdn.jsdelivr.net/gh/L1quidH2O/misc/optimized_khan_pjs/src/output.js"),
+    l("https://cdn.jsdelivr.net/gh/L1quidH2O/misc/optimized_khan_pjs/src/output_pjs.js")
   ])
   .then(()=>(pjs_onready || function(){})());
 })()
